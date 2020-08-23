07ecd1b08ea5069e095975cfcf79b61caac0d708
Jeff Reiner <gone@hey.com>
2020-08-23 12:24:29 +0200

    Working on multiple cToken mint estimations

--- api/index.js
+++ api/index.js
@@ -2,18 +2,23 @@ import { Contract as EthersContract, providers } from "ethers";
 import { parseUnits } from "ethers/lib/utils";
 // import fetchEthPrices from "../lib/fetchEthPrices";
 // import fetchGasPrices from "../lib/fetchGasPrices";
-import cToken from "../abi/cToken.json";
+import cDAI from "../abi/cDAI.json";
+import cETH from "../abi/cETH.json";
 
 const API_KEY = "dd1817d5225849d18bce57c75d4c23d1";
 
 const CONTRACTS = {
   Compound: {
-    abi: cToken,
+    abi: {
+      cDAI,
+      cETH,
+    },
   },
 };
 
 const ADDRESSES = {
   cDAI: "0x5d3a536e4d6dbd6114cc1ead35777bab948e3643",
+  cETH: "0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5",
 };
 
 export default async (req, res) => {
@@ -22,12 +27,12 @@ export default async (req, res) => {
   const Provider = new providers.InfuraProvider(1, API_KEY);
 
   try {
-    if (!(query.contract && query.function))
+    if (!(query.contract && query.function && query.token))
       throw new Error(`Parameters 'contract' & 'function' are required`);
 
     const Contract = new EthersContract(
-      ADDRESSES[query?.token ?? "DAI"],
-      CONTRACTS[query.contract].abi,
+      ADDRESSES[query.token],
+      CONTRACTS[query.contract].abi[query.token],
       Provider
     );
 
