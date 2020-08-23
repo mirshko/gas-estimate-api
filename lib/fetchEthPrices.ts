3974973c3f64d97e0c61d6b30407dc9053e1616b
Jeff Reiner <gone@hey.com>
2020-08-23 14:10:08 +0200

    Typescript functions

---
+++ lib/fetchEthPrices.ts
@@ -0,0 +1,26 @@
+type EthPricesResponse = {
+  success: boolean;
+  result?: {
+    [key: string]: number;
+  };
+  error?: string;
+  message?: string;
+};
+
+export default async function fetchEthPrices(
+  currency: "USD" | "EUR" | "GBP" = "USD"
+) {
+  try {
+    const res = await fetch("https://ethereum-api.xyz/eth-prices");
+
+    if (res.ok) {
+      const { result }: EthPricesResponse = await res.json();
+
+      return result[currency];
+    }
+
+    throw new Error("Failed to 'fetch' eth-prices");
+  } catch (error) {
+    console.error(error);
+  }
+}
---
+++ lib/fetchGasPrices.ts
@@ -0,0 +1,36 @@
+type GasPricesResponse = {
+  success: boolean;
+  result?: GasPrices;
+  error?: string;
+  message?: string;
+};
+
+interface GasPrice {
+  time: number;
+  price: number;
+}
+
+interface GasPrices {
+  timestamp: number;
+  slow: GasPrice;
+  average: GasPrice;
+  fast: GasPrice;
+}
+
+export default async function fetchGasPrices(
+  speed: "slow" | "average" | "fast" = "average"
+) {
+  try {
+    const res = await fetch("https://ethereum-api.xyz/gas-prices");
+
+    if (res.ok) {
+      const { result }: GasPricesResponse = await res.json();
+
+      return result[speed];
+    }
+
+    throw new Error("Failed to 'fetch' gas-prices");
+  } catch (error) {
+    console.error(error);
+  }
+}
